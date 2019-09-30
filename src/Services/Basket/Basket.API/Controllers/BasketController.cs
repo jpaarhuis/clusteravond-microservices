fae3edca9701b6c6ec2357425ff7170878b46d68
Jeffrey Paarhuis <jpaarhuis@home.nl>
2019-09-30 21:36:03 +0200

    styling

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -66,9 +66,22 @@ namespace Microsoft.eShopOnContainers.Services.Basket.API.Controllers
 
             var userName = User.FindFirst(x => x.Type == "unique_name").Value;
 
-            var eventMessage = new UserCheckoutAcceptedIntegrationEvent(userId, userName, basketCheckout.City, basketCheckout.Street,
-                basketCheckout.State, basketCheckout.Country, basketCheckout.ZipCode, basketCheckout.CardNumber, basketCheckout.CardHolderName,
-                basketCheckout.CardExpiration, basketCheckout.CardSecurityNumber, basketCheckout.CardTypeId, basketCheckout.Buyer, basketCheckout.RequestId, basket);
+            var eventMessage = new UserCheckoutAcceptedIntegrationEvent(
+                userId, 
+                userName, 
+                basketCheckout.City, 
+                basketCheckout.Street,
+                basketCheckout.State, 
+                basketCheckout.Country, 
+                basketCheckout.ZipCode, 
+                basketCheckout.CardNumber, 
+                basketCheckout.CardHolderName,
+                basketCheckout.CardExpiration, 
+                basketCheckout.CardSecurityNumber, 
+                basketCheckout.CardTypeId, 
+                basketCheckout.Buyer, 
+                basketCheckout.RequestId, 
+                basket);
 
             // Once basket is checkout, sends an integration event to
             // ordering.api to convert basket to order and proceeds with
